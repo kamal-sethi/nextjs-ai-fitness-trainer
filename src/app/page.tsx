1ab2957189355753c9ca9c5aa0cfa70ba10e9d40
kamal-sethi <kamalsethi018@gmail.com>
2025-06-25 14:52:56 +0530

    used clerk middleware to protect routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-
-import React from 'react'
+import { SignedIn, SignedOut, SignIn, SignInButton, SignOutButton } from "@clerk/nextjs";
+import React from "react";
 const Homepage = () => {
   return (
     <div>
       Home Page
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
